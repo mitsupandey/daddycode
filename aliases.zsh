1d6a0c8252c05f44fa2bc0043fee780b13b8f68d
buterly <butterly095@gmail.com>
2024-01-13 12:54:26 +0530

    asdf

---
+++ aliases.zsh
@@ -0,0 +1,24 @@
+alias vim=nvim
+export PATH=$HOME/.local/bin:/usr/local/bin:$PATH
+export EDITOR='nvim'
+#export PATH=$HOME/.local/bin
+
+# Path to your oh-my-zsh installation.
+alias brave-browser="brave-browser --password-store=gnome"
+alias i3conf='vim ~/.config/i3/config'
+alias gst="git status"
+alias gpush="git push origin main"
+alias gpull="git pull origin main"
+alias gcom="git commit -m"
+alias gadd="git add"
+
+alias clip="xclip -selection clipboard"
+
+alias dnfs="sudo dnf search"
+alias dnfr="sudo dnf remove"
+alias dnfi="sudo dnf install"
+
+export rockerz="FC:58:FA:C9:D9:A7"
+
+
+alias l="ls -la"
